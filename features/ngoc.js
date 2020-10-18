bb3a4024e7fddfa2525afea55f9ad3e6b64fb6ed
ngocthily <ngoctly94@gmail.com>
2020-10-18 15:52:38 -0700

    Added ngocs resume

---
+++ features/ngoc.js
@@ -0,0 +1,136 @@
+module.exports = function(controller) {
+    ngocResume = {
+        "basics": {
+            "name": "Ngoc Thi Ly",
+            "label": "Full Stack Software Engineer",
+            "picture": "",
+            "email": "ngoctly94@gmail.com",
+            "phone": "(408) 784-6545",
+            "website": "https://ngocthily.com/",
+            "summary": "Bay Area native who loves to code",
+            "location": {
+                // "address": "2712 Broadway St",
+                // "postalCode": "CA 94115",
+                "city": "San Jose",
+                "countryCode": "US",
+                "region": "California"
+            },
+            "profiles": [{
+                "network": "LinkedIn",
+                "username": "ngocthily",
+                "url": "https://www.linkedin.com/in/ngocthily/"
+            },
+            {
+                "network": "GitHub",
+                "username": "ngocthily",
+                "url": "https://github.com/ngocthily"
+            }]
+        },
+        "work": [{
+            "company": "Satellite",
+            "position": "Backend Software Developer Intern",
+            "website": "https://www.mysatellites.com/",
+            "startDate": "2020-09-01",
+            "endDate": "Current",
+            "summary": "Employed a backend API for their beta web application",
+            // "highlights": [
+            //     "Started the company"
+            // ]
+        }, {
+            "company": "DryVac",
+            "position": "Contract Software Developer",
+            "website": "http://dryvacco.com/",
+            "startDate": "2020-07-01",
+            "endDate": "2020-09-01",
+            "summary": "Implement automation programs using python and redesigned company's webpage for 2021",
+            // "highlights": [
+            //     "Started the company"
+            // ]
+        }],
+        "education": [{
+                "institution": "App Academy",
+                "area": "Immersive Full Stack Web Development",
+                "studyType": "Bootcamp",
+                "startDate": "2020-02-01",
+                "endDate": "2020-05-01",
+                "courses": [
+                    "",
+                    ""
+                ]
+            },
+            {
+            "institution": "University of California, Irvine",
+            "area": "Mathematics and Psychology",
+            "studyType": "Bachelor",
+            "startDate": "2015-09-01",
+            "endDate": "2018-06-08",
+            "gpa": "3.11",
+            "courses": [
+                "Linear Algebra",
+                "Numerical Analysis with MATLAB programming",
+                "Abstract Mathematics",
+                "Organisms to Ecosystems",
+                "Elementary Differential Equations",
+                "Probability and Stochastic Processes",
+                "Sociocultural Anthropology",
+                "Modern Geometry",
+                "Sports Psychology",
+                "Psychology in Film",
+                "Cognitive Science",
+                "Brain Disorders",
+                "Partial Differential Equations",
+                "Group Theory",
+                "Elementary Analysis",
+                "MATLAB Programming",
+                "Mathematical Modeling",
+                "Leaders and Organizational Behaviors",
+                "Statistics",
+                "Psychological Research Methods with Lab",
+                "Theory of Differential Equations",
+                "Psychology Fundamentals",
+                "Abnormal Psychology",
+                "Psychology of Music",
+                "Psychotherapy",
+                "People in Society",
+                "History of Psychology"
+            ]
+        }],
+        "awards": [{
+            "title": "Award",
+            "date": "2014-11-01",
+            "awarder": "Company",
+            "summary": "There is no spoon."
+        }],
+        "publications": [{
+            "name": "Publication",
+            "publisher": "Company",
+            "releaseDate": "2014-10-01",
+            "website": "http://publication.com",
+            "summary": "Description..."
+        }],
+        "skills": [{
+            "name": "Web Development",
+            "level": "Master",
+            "keywords": [
+                "HTML",
+                "CSS",
+                "Javascript"
+            ]
+        }],
+        "languages": [{
+            "language": "English",
+            "fluency": "Native speaker"
+        }],
+        "interests": [{
+            "name": "Wildlife",
+            "keywords": [
+                "Ferrets",
+                "Unicorns"
+            ]
+        }],
+        "references": [{
+            "name": "Jane Doe",
+            "reference": "Reference..."
+        }]
+    }
+}
